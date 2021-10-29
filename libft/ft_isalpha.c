d73e00bd923a3cced8923272e6cfea0c53e13268
BobyCharbon <boby01170@gmail.com>
2021-10-29 16:45:37 +0200

    finish isalpha (a corriger)

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -6,20 +6,31 @@
 /*   By: ltromber <ltromber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 14:57:31 by ltromber          #+#    #+#             */
-/*   Updated: 2021/10/29 15:22:45 by ltromber         ###   ########.fr       */
+/*   Updated: 2021/10/29 16:44:11 by ltromber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ctype.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int	ft_isalpha(int a);
 
 int	main(void)
 {
 	int	a;
 
+	a = ';';
 	ft_isalpha(a);
 }
 
 int	ft_isalpha(int a)
 {
-
+	if ((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'))
+	{
+		printf("Yes\n");
+		return (1);
+	}
+	printf("No\n");
+	return (0);
 }
