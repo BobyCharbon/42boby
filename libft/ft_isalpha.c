85741c2e2f7219b2dbee2568d9bbc62f5b8af4a5
BobyCharbon <boby01170@gmail.com>
2021-10-29 16:55:31 +0200

    deleted main

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -6,23 +6,13 @@
 /*   By: ltromber <ltromber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 14:57:31 by ltromber          #+#    #+#             */
-/*   Updated: 2021/10/29 16:48:34 by ltromber         ###   ########.fr       */
+/*   Updated: 2021/10/29 16:55:12 by ltromber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ctype.h>
 #include <stdio.h>
 #include <unistd.h>
-
-int	ft_isalpha(int a);
-
-int	main(void)
-{
-	int	a;
-
-	a = ';';
-	ft_isalpha(a);
-}
 
 int	ft_isalpha(int a)
 {
