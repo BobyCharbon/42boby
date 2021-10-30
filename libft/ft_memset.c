01e9c3c030fb545adbdbe8c90876648e1c5702c2
Boby <boby01170@gmail.com>
2021-10-30 15:03:00 +0200

    continue memset

---
+++ libft/ft_memset.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ludovictrombert <ludovictrombert@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/30 14:52:24 by ludovictrom       #+#    #+#             */
+/*   Updated: 2021/10/30 15:02:51 by ludovictrom      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int *ft_memset(void *s, int c, size_t  n);
+
+int main(void)
+{
+
+}
+
+int *ft_memset(void *s, int c, size_t  n)
+{
+    
+}
