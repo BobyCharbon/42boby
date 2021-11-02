a380a36e39bcdfb911c1a6db178168a6c6f0f8cc
BobyCharbonPARROT <boby01170@gmail.com>
2021-11-02 12:17:32 -0400

    finish strchr

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: kingtringer <kingtringer@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 15:37:40 by ltromber          #+#    #+#             */
-/*   Updated: 2021/11/02 11:36:06 by kingtringer      ###   ########.fr       */
+/*   Updated: 2021/11/02 12:17:24 by kingtringer      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,30 @@
 #include <stdlib.h>
 #include <string.h>
 
-int	ft_strchr(char	*str, char c, char *dest);
+char	*ft_strchr(const char	*str, int c);
 
 int	main(void)
 {
-	char	chaine[] = "Ah ouais gros tema la taille du raph";
-	char	dest[50];
+	char	chaine[] = "Ah ouais gros tema ta taille du raph";
+	//char	dest[50];
 
-	const char c = 't';
-	ft_strchr(chaine, c, dest);
+	int c = 't';
+	//ft_strchr(chaine, c);
 	printf("VRAI : %s\n\n",strchr(chaine, c));
 	//printf("Source : %s\n", chaine);
 	//printf("A Rechercher : %c\n", c);
-	printf("MOI : %s\n", dest);
+	printf("MOI : %s\n", ft_strchr(chaine, c));
 }
 
-int	ft_strchr(char	*str, char c, char *dest)
+char	*ft_strchr(const char	*str, int c)
 {
 	int	i;
-	int	j;
 
 	i = 0;
-	j = 0;
 
 	while (str[i] != c)
 	{
 		i++;
 	}
-	if (str[i] == c)
-	{
-		while (str[i])
-		{
-			dest[j] = str[i];
-			i++;
-			j++;
-		}
-		
-	}
-	
-	return (0);
+	return (str + i);
 }
