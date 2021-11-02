6dfb654526241c18739736a00251abc18eebb20a
BobyCharbonPARROT <boby01170@gmail.com>
2021-11-02 11:52:34 -0400

    add reel function to strrchr

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -3,49 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ltromber <ltromber@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kingtringer <kingtringer@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 18:28:19 by ltromber          #+#    #+#             */
-/*   Updated: 2021/11/01 18:36:44 by ltromber         ###   ########.fr       */
+/*   Updated: 2021/11/02 11:48:54 by kingtringer      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
-int	ft_strrchr(char *str, char c, char dest);
+int	ft_strrchr(char *str, char c);
 
 int main(void)
 {
 	char chaine[] = "Ah ouais gros tema la taille du raph";
-	char dest[50];
+	//char dest[50];
 
 	const char c = 't';
-	ft_strrchr(chaine, c, dest);
-	printf("%s", chaine);
+	printf("VRAI : %s\n\n", strrchr(chaine, c));
+	ft_strrchr(chaine, c);
+	printf("MOI : %s\n", ft_strrchr);
 }
 
-int	ft_strrchr(char *str, char c, char dest)
+int	ft_strrchr(char *str, char c)
 {
 	int	i;
-	int	j;
+	int	*j;
 	
 	i = 0;
 	j = 0;
 
-	while (str[i])
+	while (str[i] != c)
 	{
 		i++;
 	}
-	while (str[i] != c)
+	while (str[i] == c)
 	{
-		i--;
-		if (str[i] == c)
-		{
-			dest[j] = str[i]
-			j++;
-			i--;
-		}
+		j = i;
+		i++;
 	}
-	return (0);
+	return (str[j]);
 }
