d78be15bd08ffe461586ead1132ce8d84f61c37f
BobyCharbon <boby01170@gmail.com>
2021-11-03 18:06:29 +0100

    update memcmp

---
+++ libft/ft_memcmp.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+//#include "libft.h"
+
+int ft_memcmp(const void *s1, const void *s2, size_t n);
+
+int main(void)
+{
+    int s1[] = { 54, 85, 20 };
+    int s2[] = { 53, 80, 21 };
+    
+    size_t n;
+    n = 20;
+
+    printf("VRAI : %d\n", memcmp(s1, s2, n));
+}
+
+
+int ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+
+}
