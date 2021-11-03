1c9093d815dede40f37f40b475d39fe65605487c
BobyCharbon <boby01170@gmail.com>
2021-11-03 22:20:30 +0100

    update bzero

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -3,29 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ltromber <ltromber@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kingtringer <kingtringer@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 12:06:34 by ltromber          #+#    #+#             */
-/*   Updated: 2021/11/01 12:20:12 by ltromber         ###   ########.fr       */
+/*   Updated: 2021/11/03 19:44:09 by kingtringer      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <strings.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 void	ft_bzero(void *s, size_t n);
 
 int main(void)
 {
-	ft_bzero();
-}
+	char s[] = "Salut";
 
+	size_t n;
+	n = 10;
+	printf("VRAI : %s\n", bzero(s, n));
+	//ft_bzero(s, n);
+}
+/* 
 void	ft_bzero(void *s, size_t n)
 {
-	char	chaine[10];
-	site_t n = 10;
-}
 
+}
+ */
 /* 
 int main(void)
 {
