7fea76a38e93dadd29ebaef032fca532f7f511be
BobyCharbon <boby01170@gmail.com>
2021-11-04 16:08:31 +0100

    finish memcpy(fix a bug after)

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -8,52 +8,29 @@ void *ft_memcpy(void *dest, const void *src, size_t n);
 
 int main(void)
 {
-    char src[] = "YYYYYYYYYYYYYYYYYYYYYYYY";
-    char src_test[] = "YYYYYYYYYYYYYYYYYYYYY";
-    char dest[] = "XXXXXXXXXXXXXXXXXXXX";
-    char dest_test[] = "XXXXXXXXXXXXXXXXXXXX";
+    char src[] = "YYYYYYYY";
+    char src_test[] = "YYYYYYYY";
+    char dest[] = "XXXXXXXX";
+    char dest_test[] = "XXXXXXXX";
     size_t n;
     
-    n = 10;
+    n = 20;
     printf("VRAI : %s\n",memcpy(dest_test, src_test, n));
     printf("MOI : %s\n", ft_memcpy(dest, src, n));
 }
 
 void *ft_memcpy(void *dest, const void *src, size_t n)
 {
-
-    /*
     size_t i;
-    size_t j;
-    size_t l;
 
-    const char *source;
-    const char *destination;
-    source = src;
-    destination = dest;
-    char destination;
-    destination = *dest;
-    l = 0;
     i = 0;
-    j = 0;
-    */
-
     char *tmp = dest;
     const char *source = src;
-    
+
     while (n--)
-    {
+    {                      
         *tmp++ = *source++;
+        i++;
     }
     return dest;
-    
-
-    /* 
-    while (destination[j] || (i < n))
-    {
-        destination[j] = src + i;
-        i++;
-        j++;
-    }
-     */
 }
