5ed5294bd63009a144d122b5bb8eff7edd915779
BobyCharbon <boby01170@gmail.com>
2021-11-04 01:00:44 +0100

    finish memset

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ludovictrombert <ludovictrombert@studen    +#+  +:+       +#+        */
+/*   By: kingtringer <kingtringer@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 14:52:24 by ludovictrom       #+#    #+#             */
-/*   Updated: 2021/10/30 15:02:51 by ludovictrom      ###   ########.fr       */
+/*   Updated: 2021/11/04 01:00:28 by kingtringer      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,34 @@ int *ft_memset(void *s, int c, size_t  n);
 
 int main(void)
 {
+    char h[] = "Salut";
 
+    size_t n;
+    int c;
+    
+    n = 2;
+    c = 'X';
+    memset(h, c, n);
+    char s[] = "Salut";
+    ft_memset(s, c, n);
 }
 
 int *ft_memset(void *s, int c, size_t  n)
 {
-    
+    char *str;
+    size_t i;
+    size_t j;
+
+    str = s;
+    i = 0;
+    j = 0;
+
+    while (str[i] && i < n)
+    {
+        str[i] = c;
+        i++;
+        j++;
+    }
+    return (0);
+
 }
