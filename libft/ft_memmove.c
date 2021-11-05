2da15df837c99b45b7755bfc4aefc80e64123346
BobyCharbon <boby01170@gmail.com>
2021-11-05 15:49:48 +0100

    update memmove

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -4,20 +4,39 @@
 
 //#include "libft.h"
 
-//void *ft_memmvoe(void *dst, const void *src, size_t len);
+void *ft_memmove(void *dst, const void *src, size_t len);
 
 int main(void)
 {
-	char src[] = "Tema la taille xxxxxxxxxxxxxx";
+	char src[] = "Tema la taille";
 	char dst[] = "du raph";
 
 	size_t len;
-	len = 10;
+	len = 5;
 	printf("VRAI : %s\n", memmove(dst, src, len));
-	//ft_memmvoe(dst, src, len);
+	ft_memmove(dst, src, len);
 }
-/* 
-void *ft_memmvoe(void *dst, const void *src, size_t len)
+ 
+void *ft_memmove(void *dst, const void *src, size_t len)
 {
+	char *dest;
+	const char *s;
 
-} */
+	size_t i;
+
+	s = src;
+	char *tmp;
+	tmp = NULL;
+
+
+	dest = dst;
+	i = 0;
+
+while (s[i] || len--)
+	{
+		tmp[i] = s[i];
+		i++; 
+	}
+	
+	return (dst);
+}
