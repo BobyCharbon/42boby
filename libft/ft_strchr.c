c1e12c5db91604e26c528470ecc48cd62a8da618
Boby <boby01170@gmail.com>
2021-11-10 16:06:06 +0100

    add libft.h

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -3,31 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kingtringer <kingtringer@student.42.fr>    +#+  +:+       +#+        */
+/*   By: ludovictrombert <ludovictrombert@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 15:37:40 by ltromber          #+#    #+#             */
-/*   Updated: 2021/11/02 12:17:24 by kingtringer      ###   ########.fr       */
+/*   Updated: 2021/11/10 16:05:20 by ludovictrom      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-char	*ft_strchr(const char	*str, int c);
-
-int	main(void)
-{
-	char	chaine[] = "Ah ouais gros tema ta taille du raph";
-	//char	dest[50];
-
-	int c = 't';
-	//ft_strchr(chaine, c);
-	printf("VRAI : %s\n\n",strchr(chaine, c));
-	//printf("Source : %s\n", chaine);
-	//printf("A Rechercher : %c\n", c);
-	printf("MOI : %s\n", ft_strchr(chaine, c));
-}
+#include "libft.h"
 
 char	*ft_strchr(const char	*str, int c)
 {
@@ -40,4 +26,17 @@ char	*ft_strchr(const char	*str, int c)
 		i++;
 	}
 	return (str + i);
+}
+
+int	main(void)
+{
+	char	chaine[] = "Ah ouais gros tema ta taille du raph";
+	//char	dest[50];
+
+	int c = 't';
+	//ft_strchr(chaine, c);
+	printf("VRAI : %s\n\n",strchr(chaine, c));
+	//printf("Source : %s\n", chaine);
+	//printf("A Rechercher : %c\n", c);
+	printf("MOI : %s\n", ft_strchr(chaine, c));
 }
