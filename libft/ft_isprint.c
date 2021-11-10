70f765541ee7853e2fc2742a8925e4704bb99d42
Boby <boby01170@gmail.com>
2021-11-10 14:03:21 +0100

    update isprint + add libft.h

--- libft/ft_isprint.c
+++ libft/ft_isprint.c
@@ -3,14 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ltromber <ltromber@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ludovictrombert <ludovictrombert@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 19:34:50 by ltromber          #+#    #+#             */
-/*   Updated: 2021/10/29 19:45:20 by ltromber         ###   ########.fr       */
+/*   Updated: 2021/11/10 14:00:26 by ludovictrom      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_isprint(int a)
 {
 	return (a >= 0 && a <= 126);
+}
+
+int main(void)
+{
+	int a;
+	a = 'd';
+	ft_isprint(a);
 }
