25dfebe18d792f46e94c00290efd87991da488b0
Boby <boby01170@gmail.com>
2021-11-10 14:42:08 +0100

    update isalnum + add libft.h

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -3,16 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ltromber <ltromber@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ludovictrombert <ludovictrombert@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 17:32:40 by ltromber          #+#    #+#             */
-/*   Updated: 2021/10/29 17:40:25 by ltromber         ###   ########.fr       */
+/*   Updated: 2021/11/10 14:41:53 by ludovictrom      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ctype.h>
+#include <stdio.h>
+#include "libft.h"
 
-ft_isalnum(int a)
+int ft_isalnum(int a)
 {
 	return ((a >= '0' && a <= '9') || (a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'));
+}
+
+int main(void)
+{
+	int a = 'd';
+	printf("Résultat : %d", ft_isalnum(a));
 }
