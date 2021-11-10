d354988866863d2259e42d90e07990b1f2fbd84f
Boby <boby01170@gmail.com>
2021-11-10 19:45:46 +0100

    Auto stash before merge of "memmove" and "origin/memmove"

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -1,26 +1,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
-
-//#include "libft.h"
-
-void *ft_memmove(void *dst, const void *src, size_t len);
-
-int main(void)
-{
-	char src[] = "12345";
-	char dst[] = "6789";
-
-	char srctest[] = "12345";
-	char dsttest[] = "6789";
-
-	size_t len;
-	len = 5;
-	printf("VRAI : %s\n\n", memmove(dsttest, srctest, len));
-	printf("Source : %s\n", src);
-	printf("Destination : %s\n\n", dst);
-	printf("MOI : %s\n\n",ft_memmove(dst, src, len));
-}
+#include "libft.h"
  
 void *ft_memmove(void *dst, const void *src, size_t len)
 {
@@ -35,12 +16,29 @@ void *ft_memmove(void *dst, const void *src, size_t len)
 
 	while (len--)
 	{
-		if (tmp[j])
+		if (src[i])
 		{
-			dest[i] = tmp[j];
+			*(char*) dst + i = *(char*) src + i;
+			// dest[i] = tmp[j];
 			i++;
 			j++;
 		}
 	}
 	return (dest);
+}
+
+int main(void)
+{
+	char src[] = "12345";
+	char dst[] = "6789";
+
+	char srctest[] = "12345";
+	char dsttest[] = "6789";
+
+	size_t len;
+	len = 5;
+	printf("VRAI : %s\n\n", memmove(dsttest, srctest, len));
+	// printf("Source : %s\n", src);
+	// printf("Destination : %s\n\n", dst);
+	printf("MOI : %s\n\n",ft_memmove(dst, src, len));
 }
