8f6d07a180bbde7ad9bb67e6ead4b129002ddbe0
Boby <boby01170@gmail.com>
2021-11-10 13:52:51 +0100

    add include libft

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -1,8 +1,8 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
+#include "libft.h"
 
-/* 
 void *ft_memchr(const void *str, int c, size_t n)
 {
     size_t i;
@@ -24,4 +24,4 @@ void *ft_memchr(const void *str, int c, size_t n)
     }
     return (NULL);
 }
-     */
+
