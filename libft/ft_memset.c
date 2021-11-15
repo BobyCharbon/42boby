f85a47e5dc28c3c5c338e0531769c9e31c6455be
Boby <boby01170@gmail.com>
2021-11-15 19:01:30 +0100

    update memset (normally OK)

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -3,49 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kingtringer <kingtringer@student.42.fr>    +#+  +:+       +#+        */
+/*   By: ludovictrombert <ludovictrombert@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 14:52:24 by ludovictrom       #+#    #+#             */
-/*   Updated: 2021/11/04 01:00:28 by kingtringer      ###   ########.fr       */
+/*   Updated: 2021/11/15 19:00:27 by ludovictrom      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include "libft.h"
 
-int *ft_memset(void *s, int c, size_t  n);
+void *ft_memset(void *s, int c, size_t  n)
+{
+    char *str;
+    size_t i;
 
+    str = s;
+    i = 0;
+
+    while (n--)
+    {
+        str[i] = c;
+        i++;
+    }
+    return ((char*)str);
+
+}
+/* 
 int main(void)
 {
-    char h[] = "Salut";
+    char h[] = "A";
+    char s[] = "A";
 
     size_t n;
     int c;
     
     n = 2;
     c = 'X';
-    memset(h, c, n);
-    char s[] = "Salut";
-    ft_memset(s, c, n);
+    printf("VRAI : %s\n", memset(h, c, n));
+    printf("Moi : %s\n", ft_memset(s, c, n));
 }
-
-int *ft_memset(void *s, int c, size_t  n)
-{
-    char *str;
-    size_t i;
-    size_t j;
-
-    str = s;
-    i = 0;
-    j = 0;
-
-    while (str[i] && i < n)
-    {
-        str[i] = c;
-        i++;
-        j++;
-    }
-    return (0);
-
-}
+ */
