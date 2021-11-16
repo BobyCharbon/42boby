bb9fecb8499c8effe48453675b6b761cec6f02f7
Boby <boby01170@gmail.com>
2021-11-16 16:15:22 +0100

    update the variable of strlcat

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -18,32 +18,34 @@ size_t ft_strlcat(char *dest, const char *src, size_t len)
 
     j = dest_len - src_len;
 
-    if(!*dest || !*src || len < dest_len || len <= 0)
-        return (src_len + len);
-
+    // if(!*dest || !*src || len <= 0)
+    if (dest_len <= len)
+        res = dest_len + src_len;
+    else
+        res = src_len + len;
     while (j < dest_len && i < src_len)
     {
-        dest[j] = src[i];
+        ((unsigned char*) dest)[j] = ((unsigned char*) src)[i];
         j++;
         i++;
     }
     dest[j] = 0;
-    return (src_len + dest_len);
+    return (res);
 }
 
-/* 
+
 int main(void)
 {
-    char dest [15] = "XXXXXXXXXX";
+    char dest [] = "XXXXXXXXXX";
     char src [] = "YYYYY";
 
-    char dest_test [15] = "XXXXXXXXXX";
+    char dest_test [] = "XXXXXXXXXX";
     char src_test [] = "YYYYY";
 
-    size_t len = 4;
+    size_t len = 1;
 
-    printf("Moi: %zu\n", ft_strlcat(dest, src, sizeof(dest)));
-    printf("VRAI: %lu\n", strlcat(dest_test, src_test, sizeof(dest_test)));
+    printf("Moi: %zu\n", ft_strlcat(dest, src, len));
+    printf("VRAI: %zu\n", strlcat(dest_test, src_test, len));
     printf("\ndest_test : %s\n", dest_test);
-    printf("dest      : %s\n", dest_test);
-} */
+    printf("dest      : %s\n", dest);
+}
