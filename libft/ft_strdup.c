181f26e48505dbd26d551629e3bdd10c239b7202
Boby <boby01170@gmail.com>
2021-11-17 16:42:53 +0100

    finish strdup (passed test)

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -7,19 +7,21 @@
 char *ft_strdup(const char *str) 
 {
     char *str_duplicate;
+    int str_len;
 
-    str_duplicate = malloc(sizeof(str));
+    str_len = strlen(str);
+    str_duplicate = malloc(strlen (str) + 1);
 
-    if (str_duplicate == NULL)
-        return (str_duplicate);
-
+    if (!str_duplicate)
+        return (NULL);
     strcpy(str_duplicate, str);
     return (str_duplicate);
 }
+
 /* 
 int main(void)
 {
-    char str[] = "Salut mec";
+    char str[] = "this is a normal test";
     ft_strdup(str);
     // printf("str_duplicate: %s\n", str_duplicate);
 } */
