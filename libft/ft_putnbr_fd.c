7432b89833a572dbc790bc798164281b7f839d99
Boby <boby01170@gmail.com>
2021-11-29 13:30:09 +0100

    finished putnbr_fd (tests failed)

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -3,17 +3,37 @@
 #include <unistd.h>
 #include "libft.h"
 
+/* -------------------------------------------------------------------------- */
+/*                     Ecrit l'integer 'n' sur le fd donné                    */
+/* -------------------------------------------------------------------------- */
+/* ----------------------------- utiliser write ----------------------------- */
+/* ------------------ convertir les int en char pour write ------------------ */
+/* -------------------------------------------------------------------------- */
+
+
+
+
 void ft_putnbr_fd (int n, int fd)
 {
-    int i;
-
-    i = 0;
-
-    write(fd, &n, 1);
+    if (n < 0)
+    {
+        ft_putchar_fd('-', fd);
+        if (n < -9)
+            ft_putnbr_fd(n / -10, fd);
+        ft_putnbr_fd(-(n % 10), fd);
+    }
+    else if (n >= 0 && n <= 9)
+        ft_putchar_fd (n + 48, fd);
+    else
+    {
+        ft_putchar_fd (n / 10, fd);
+        ft_putchar_fd (n % 10, fd);
+    }
 }
 /* 
 int main(void)
 {
-    int n = 1;
+    int n = -8;
+    int fd;
     ft_putnbr_fd(n, fd);
 } */
