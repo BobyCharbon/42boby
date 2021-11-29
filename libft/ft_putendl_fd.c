6993c8efb8abf2445306e9fe2546c183f0165f9d
Boby <boby01170@gmail.com>
2021-11-29 12:03:34 +0100

    finish putendl_fd (tests OK)

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -11,11 +11,21 @@
 
 void ft_putendl_fd (char *s, int fd)
 {
+    int i;
 
+    i = 0;
+    if (!fd || !s)
+        return;
+    while (((char*)s)[i])
+    {
+        write(fd, s + i, 1);
+        i++;
+    }
+    write (fd, "\n", 1);
 }
-
+/* 
 int main(void)
 {
     char s[] = "Salut";
     
-}
+} */
