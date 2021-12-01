32e0fd6287e0ea83208a3d6abb995b9e0f720d94
Boby <boby01170@gmail.com>
2021-12-01 13:08:40 +0100

    created strmapi

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+//#include "libft.h"
+
+/* -------------------------------------------------------------------------- */
+/*         Applique la fonction 'f' à chaque caractère de la chaine de        */
+/* ---- cractère de la chaine de caractères passée en argument pour créer --- */
+/* ------------- une nouvelle chaine de caractères (avec malloc) ------------ */
+/* -------------- successives de 'f' résultant des applications ------------- */
+/* -------------------------------------------------------------------------- */
+/* ------------ *s = la chaine de caractères sur laquelle itérer ------------ */
+/* ------------- *f = la fonction à appliquer à chaque caractère ------------ */
+/* -------------------------------------------------------------------------- */
+/* ---------------------------- VALEUR DE RETOUR: --------------------------- */
+/* ----- la chaine de caractères résultant des applications successives ----- */
+/* ---------------------------------- de f. --------------------------------- */
+/* ------------------ retourne NULL si l'allocation échoue ------------------ */
+/* -------------------------------------------------------------------------- */
+
+
+
+char *ft_strmapi(const char *s, char (*f) (unsigned int, char))
+{
+
+}
+
+int main(void)
+{
+
+}
