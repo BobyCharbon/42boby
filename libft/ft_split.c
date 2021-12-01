12e2d2218649a10892ea81573c20ad7c87a1cc87
Boby <boby01170@gmail.com>
2021-12-01 11:13:54 +0100

    finished split (tests OK but not protected)

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-//#include "libft.h"
+#include "libft.h"
 
 /* -------------------------------------------------------------------------- */
 /*            Fonction allouant avec Malloc et retourne un tableau            */
@@ -11,54 +11,105 @@
 /* -------------------------------------------------------------------------- */
 /* ------------------ le tableau doit se terminer par NULL ------------------ */
 /* -------------------------------------------------------------------------- */
+/* ---------------------------- VALEUR DE RETOUR: --------------------------- */
+/* -------------- le tableau de nouvelles chaines de caracères -------------- */
+/* ------------------------- résultant du découpage. ------------------------ */
+/* ----------------------- NULL si l'allocation échoue ---------------------- */
+/* -------------------------------------------------------------------------- */
 
-static size_t find_and_split(char *s, char c, size_t x)
+/* -------------------------------------------------------------------------- */
+/*                          alloue la fonction split                          */
+/* -------------------------------------------------------------------------- */
+static char **ft_malloc_split(char const *s, char c)
 {
-    while (s[x] || s[x] != c)
+    size_t i;
+    char **split;
+    size_t total;
+
+    i = 0;
+    total = 0;
+    while (s[i])
     {
-        x++;
-        if (s[x] == c)
-        {
-            /* ------------------------------ pas sur du \0 ----------------------------- */
-            // s[x] = 0;
-            /* -------------------------------------------------------------------------- */
-            return (x);
-        }
+        if (s[i] == c)
+            total++;
+        i++;
     }
-    return (0);
+    split = (char**)malloc(sizeof(s) * (total + 2));
+    if (!split)
+        return (NULL);
+    return (split);
+}
+
+/* -------------------------------------------------------------------------- */
+/*                          free la mémoire de split                          */
+/* -------------------------------------------------------------------------- */
+void *ft_free_split(char **split, size_t elts)
+{
+    size_t i;
+
+    i = 0;
+    while (i < elts)
+    {
+        free(split[i]);
+        i++;
+    }
+    free(split);
+    return (NULL);
+}
+
+
+static void *ft_split_range(char**split, const char *s, t_split_next *st, t_split_next *lt)
+{
+    split[lt->length] = ft_substr(s, st->start, st->length);
+    if (!split[lt->length])
+        return (ft_free_split(split, lt->length));
+    lt->length++;
+    return (split);
+}
+
+static void *ft_split_by_char(char **split, const char *s, char c)
+{
+    size_t i;
+    t_split_next st;
+    t_split_next lt;
+
+    i = 0;
+    lt.length = 0;
+    lt.start = 0;
+    while(s[i])
+    {
+        if (s[i] == c)
+        {
+            st.start = lt.start;
+            st.length = (i - lt.start);
+            if (i > lt.start && !ft_split_range(split, s, &st, &lt))
+                return (NULL);
+            lt.start = i + 1;
+        }
+        i++;
+    }
+    st.start = lt.start;
+    st.length = (i - lt.start);
+    if (i > lt.start && i > 0 && !ft_split_range(split, s, &st, &lt))
+        return (NULL);
+    split[lt.length] = 0;
+    return (split);
 }
 
 char **ft_split(const char *s, char c)
 {
-    size_t i ;
-    size_t x ;
     char **split;
-    char *s2_duplicate;
-    char *s_duplicate;
-    i = 0;
-    x = 0;
-
-    s2_duplicate = malloc(sizeof(char));
-    s_duplicate = malloc(sizeof(char));
-    x = find_and_split((char*) s, c, x);
-   
-    while (s[i] != s[x])
-    {
-        s_duplicate[i] = ((char*)s)[i];
-        i++;
-    }
-    while (s[i])
-    {
-        s2_duplicate[i] = ((char*)s)[i];
-        i++;
-    }
-    // split = malloc(sizeof(s_duplicate + s2_duplicate + 1));
-    // return (s_duplicate);
+    
+    if (!(split = ft_malloc_split(s, c)))
+        return (NULL);
+    if (!ft_split_by_char(split, s, c))
+        return (NULL);
+    return (split);
 }
 
-int main(void)
-{
-    char s[] = "Salut mec";
-    char c = ' ';
-    printf("RESULTAT : |%s|\n", ft_split(s, c));
-}
+// int main(void)
+// {
+//     char s[] = "Salut mec";
+//     char c = ' ';
+//     printf("RESULTAT : |%s|\n", ft_split(s, c));
+// }
