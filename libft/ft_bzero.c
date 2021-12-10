8aa80b19091378bfaddb44518c7f40f24e92e5f4
BobyCharbon <boby01170@gmail.com>
2021-12-10 22:13:43 +0100

    corriged bzero again

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: ltromber <ltromber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 12:06:34 by ltromber          #+#    #+#             */
-/*   Updated: 2021/12/10 22:05:42 by ltromber         ###   ########.fr       */
+/*   Updated: 2021/12/10 22:13:12 by ltromber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,23 +21,6 @@
 void	ft_bzero(void *str, size_t n)
 {
 	ft_memset(str, 0, n);
-	ft_st
-	/* size_t	i;
-	size_t	j;
-	char	*ptr;
-
-	i = 0;
-	j = 0;
-	ptr = str;
-	if (n <= 0)
-		return (*str);
-	while (j < (n - 1))
-	{
-		j = i;
-		ptr[i] = 0;
-		ptr[i] = ptr[j];
-		i++;
-	} */
 }
 
 // int main (void)
